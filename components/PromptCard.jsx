4bc29c52e806f291ea612bc4cf7fc8c867a76dc9
Neeraj Walia <nwalia.ez@gmail.com>
2023-05-07 04:53:33 +0530

    add hashtag

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -57,7 +57,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 className="font-inter text-sm blue_gradient cursor-pointer"
                 onClick={() => handleTagClick && handleTagClick(post.tag)}
             >
-                {post.tag}
+                #{post.tag}
             </p>
             {session?.user.id === post.creator._id &&
                 pathName === '/profile' && (
